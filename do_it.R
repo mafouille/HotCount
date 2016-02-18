12334b8243698aebf800d5b3817dede1bea503f9
Martin Figeac <martin.figeac@univ-lille2.fr>
2016-02-18 12:00:19 +0100

    Ajout de la licence Gnu GPL

--- do_it.R
+++ do_it.R
@@ -1,4 +1,34 @@
-version=0.1
+    ###############################################################
+    #                 This is HotCount verion 0                   #
+    #                                                             #
+    # released under GNU General Public Licence (see LICENCE.md)  #
+    #                                                             #
+    #                                                             #
+    #                                                             #
+    ###############################################################
+
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ # WARNING: doMreps use a slightly modified version of mreps. 
+ # The original version should be downloaded from 
+ #  http://mreps.univ-mlv.fr/.
+ # The aim of the modification was to have a less verbose output to 
+ # accelerate the processing (a more specific output for fastq data)
+ # 
+ # mreps is released by the author under the GNU GPL licence 
+ #  (see LICENCE.md)
+version=0
 ## usage : Rscript do_it.R design.txt CTL MUT {MUT}
 ##
 ## usage : Rscript do_it.R count.txt ALL delC dupC
--- do_it_gz.sh
+++ do_it_gz.sh
@@ -1,6 +1,37 @@
+    ###############################################################
+    #                 This is HotCount verion 0                   #
+    #                                                             #
+    # released under GNU General Public Licence (see LICENCE.md)  #
+    #                                                             #
+    #                                                             #
+    #                                                             #
+    ###############################################################
+
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ # WARNING: doMreps use a slightly modified version of mreps. 
+ # The original version should be downloaded from 
+ #  http://mreps.univ-mlv.fr/.
+ # The aim of the modification was to have a less verbose output to 
+ # accelerate the processing (a more specific output for fastq data)
+ # 
+ # mreps is released by the author under the GNU GPL licence 
+ #  (see LICENCE.md)
+
 #!/bin/bash -f
 version=0.0
-## usage : ./do_it.sh design.txt *.fastq.gz
+## usage : ./do_it_gz.sh design.txt *.fastq.gz
 
 d=`date`
 echo "## script do_it.sh running in version $version"
